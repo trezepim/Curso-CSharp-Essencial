334da6734b4899f88973b273a5049ee0de92c03c
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-11 00:43:31 -0300

    testando variáveis string, object e dynamic

---
+++ Primeiros testes/CSFundamentos1/TiposDeDados4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Definindo variáveis do tipo string, object e dynamic");
+
+string nome = "Rafael";
+string sobrenome = "Pim";
+
+Console.WriteLine(nome);
+Console.WriteLine(sobrenome);
+
+// StringBuilder
+object nota = 10;
+object valor1 = 8.55m;
+object nome1 = "Rafael Pim";
+object ativa = true;
+object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+
+Console.ReadLine();
