7119c551ba8f9bd24d79e886779c6a124f14b079
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-11 15:20:24 -0300

    testando nullable types

---
+++ Primeiros testes/CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Nullable Types");
+
+int? i = null;
+double? j = null;
+bool? k = null;
+
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+
+int? a = null;
+int b = a ?? 0;
+
+Console.WriteLine(b);
+
+int? x = 4;
+int y = 3;
+int? z = x * y;
+
+Console.WriteLine(z);
+
+int? p = 100;
+
+if (p.HasValue)
+{
+    Console.WriteLine($"p = {p.Value}");
+}
+else
+{
+    Console.WriteLine("p é nulo");
+}
+
+Console.ReadKey();
