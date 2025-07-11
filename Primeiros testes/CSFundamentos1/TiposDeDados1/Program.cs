158cd19c2a7e344706333d16fdda61383ab99479
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-10 22:03:51 -0300

    add files

---
+++ Primeiros testes/CSFundamentos1/TiposDeDados1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = 2147483647;
+uint valor4 = 2147483647;
+long valor5 = -2147483689;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+
+Console.ReadLine();
---
+++ Primeiros testes/MinhaSolucao/Meu Projeto/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Informe o nome do aluno: ");
+string? nome = Console.ReadLine();
+
+Console.WriteLine("Informe a primeira nota: ");
+int nota1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Informe a segunda nota: ");
+int nota2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Informe a terceira nota: ");
+int nota3 = int.Parse(Console.ReadLine() ?? "0");
+
+double media = (nota1 + nota2 + nota3) / 3.0;
+
+Console.WriteLine("A média do aluno " + (nome ?? "Desconhecido") + " é de: " + media);
