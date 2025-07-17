3f596b9ea9b1751e2e12d948acf52c434428f856
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-16 23:28:34 -0300

    exercício prático simples

---
+++ Primeiros testes/CSFundamentos1/ExercicioPratico3/Program.cs
@@ -0,0 +1,16 @@
+﻿int potencia = 10;
+
+Potencias potencias = new Potencias();
+Console.WriteLine(potencias.AumentarPotencia(potencia));
+
+Console.ReadKey();
+
+public class Potencias
+{
+    public int AumentarPotencia(int potencia)
+    {
+        {
+            return potencia + 3;
+        }
+    }
+}
