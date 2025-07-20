747a334a1a72270aac0e1032d46a1b78f650644e
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-20 11:03:34 -0300

    argumentos nomeados

---
+++ Primeiros testes/CSFundamentos1/ArgumentosNomeados/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Argumentos nomeados");
+
+Email email = new();
+
+email.Enviar(titulo: "Urgente", destino: "teste@email.com", assunto: "Reunião de orçamento");
+
+Console.ReadKey();
+
+public class Email
+{
+    public void Enviar(string destino, string titulo, string assunto)
+    {
+        Console.WriteLine($"Para {destino} - {titulo} \nAssunto: {assunto}");
+    }
+}
