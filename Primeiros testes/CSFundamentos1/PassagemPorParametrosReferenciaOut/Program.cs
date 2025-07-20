830b2e27fef6710423636a3d52cb4470eeb39da6
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-20 10:42:41 -0300

    calcular área e perímetro

---
+++ Primeiros testes/CSFundamentos1/PassagemPorParametrosReferenciaOut/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Argumentos por referência - out");
+
+Console.WriteLine("Informe o raio do círculo");
+double raio = Convert.ToDouble(Console.ReadLine());
+
+Circulo circulo = new();
+
+double perimetro = circulo.CalcularPerimetro(raio);
+double area = circulo.CalcularArea(raio);
+
+Console.WriteLine($"Perímetro do círculo: {perimetro}");
+Console.WriteLine($"Área do círculo: {area}");
+
+Console.ReadKey();
+
+public class Circulo
+{
+    public double CalcularArea(double raio)
+    {
+        double area = Math.PI * Math.Pow(raio, 2);
+        return area;
+    }
+    public double CalcularPerimetro(double raio)
+    {
+        double perimetro = 2 * Math.PI * raio;
+        return perimetro;
+    }
+}
