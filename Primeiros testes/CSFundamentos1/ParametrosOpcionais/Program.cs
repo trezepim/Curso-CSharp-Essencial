a4a047ce60a26f267349c52585310fd427300a05
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-22 20:20:38 -0300

    testando parâmetros opcionais

---
+++ Primeiros testes/CSFundamentos1/ParametrosOpcionais/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Parâmetros opcionais");
+
+Console.WriteLine("Informe o destino: ");
+var destino = Console.ReadLine();
+
+Console.WriteLine("Informe o título: ");
+var titulo = Console.ReadLine();
+
+Console.WriteLine("Informe o assunto: ");
+var assunto = Console.ReadLine();
+
+Email email = new();
+
+email.Enviar(destino);
+email.Enviar("teste@email.com");
+
+Console.ReadKey();
+
+public class Email
+{
+    public void Enviar(string destino, string titulo = "Sem título", string assunto = "Sem assunto")
+    {
+        Console.WriteLine($"Para {destino} - {titulo} \nAssunto: {assunto}");
+    }
+}
