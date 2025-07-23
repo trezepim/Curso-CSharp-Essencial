99de95bee196a8bd544a9d0c1d57833c9ff4d257
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-23 20:52:08 -0300

    campos estáticos

---
+++ Primeiros testes/CSFundamentos1/CamposEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Campos estáticos");
+
+A a1 = new A();
+a1.x = 10;
+
+A a2 = new A();
+a2.x = 20;
+
+A.y = 35;
+
+Console.WriteLine($"a1.x = {a1.x}");
+Console.WriteLine($"a2.x = {a2.x}");
+Console.WriteLine($"A.y = {A.y}");
+
+Console.ReadKey();
+
+public class A
+{
+    public int x;
+    public static int y;
+}
