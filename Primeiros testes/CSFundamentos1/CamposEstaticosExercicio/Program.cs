f2337ac13b32a834dc97ace62365c778e09f366e
Rafael Pim Santos <rafasantos.rps8@gmail.com>
2025-07-23 22:15:44 -0300

    exercício campos estáticos

---
+++ Primeiros testes/CSFundamentos1/CamposEstaticosExercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Campos estáticos - exercício\n");
+
+ContaCorrente c1 = new();
+c1.Conta = 101;
+c1.Nome = "Maria";
+
+ContaCorrente c2 = new();
+c2.Conta = 102;
+c2.Nome = "João";
+
+ContaCorrente.Juros = 4.10f;
+
+Console.WriteLine($"Cliente: {c1.Nome}\nJuros anual: {c1.JurosAnual()}\n");
+Console.WriteLine($"Cliente: {c2.Nome}\nJuros anual: {c2.JurosAnual()}\n");
+
+Console.ReadKey();
+
+public class ContaCorrente
+{
+    public int Conta;
+    public string? Nome;
+    public static float Juros;
+
+    public float JurosAnual()
+    {
+        return Juros * 12;
+    }
+}
